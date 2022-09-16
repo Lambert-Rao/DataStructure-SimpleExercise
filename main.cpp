b55aa168db39d2269fc34e9298593810098183ae
Lambert <lambert-teddy-smith@outlook.com>
2022-09-16 18:32:07 +0800

    added the skiplist, 30% finished

--- main.cpp
+++ main.cpp
@@ -8,10 +8,17 @@
 #include "stack_application.h"
 #include "queue_application.h"
 #include "queue.h"
+#include "sorted_chain.h"
+#include "skip_list.h"
 
 using namespace std;
 
 int main() {
-    queue_application_maze::Test();
+    skipList<int, string> sl(15,102421312);
+    cout<<sl.randomHeight()<<endl;
+    skipList<int, string> s11(15,151255);
+    cout<<s11.randomHeight()<<endl;
+    skipList<int, string> s111(15,125155);
+    cout<<s111.randomHeight()<<endl;
 
 }
--- skip_list.h
+++ skip_list.h
@@ -5,16 +5,78 @@
 #pragma once
 
 #include <utility>
+#include <cmath>
+#include <random>
+
+template<typename key, typename value>
+class skipList;
 
 template<typename key, typename value>
 class skipNode
 {
+    friend class skipList<key,value>;
     using ElmType = std::pair<const key, value>;
     ElmType element;
     skipNode<key, value> **next;
-
     skipNode(const ElmType &e, int height) : element(e)
     {
         next = new skipNode<key, value> *[height];
     }
+    explicit skipNode(int height): next(new skipNode<key, value> *[height]) {}
 };
+
+template<typename key, typename value>
+class skipList
+{
+    using ElmType = std::pair<const key, value>;
+public:
+    skipList(key largekey,int maxpairs);
+    ElmType *find(const key &k) const;
+    int randomHeight();
+private:
+    skipNode<key,value> *sentinal= nullptr;
+    skipNode<key,value> *tail=nullptr;
+    int size=0;
+    int height=0;
+    int maxheight=0;
+    key largekey;
+    skipNode<key,value> **last= nullptr;
+};
+
+template<typename key, typename value>
+skipList<key, value>::skipList(key largekey, int maxpairs):largekey(largekey)
+{
+    maxheight=log2(maxpairs);
+    sentinal=new skipNode<key,value>(maxheight+1);
+    last=new skipNode<key,value>*[maxheight+1];
+}
+
+template<typename key, typename value>
+std::pair<const key, value>* skipList<key, value>::find(const key &k) const
+{
+    if(k>=largekey)
+        return nullptr;
+    skipNode<key,value> *p=sentinal;
+    for(int i=height;i>=0;i--)
+    {
+        while(p->next[i]->element.first<k)
+            p=p->next[i];
+    }
+    if(p->next[0]->element.first==k)
+        return &p->next[0]->element;
+    else
+        return nullptr;
+}
+
+template<typename key, typename value>
+int skipList<key, value>::randomHeight()
+{
+    static default_random_engine e(time(nullptr));
+    static uniform_int_distribution<unsigned> u(0,1);
+
+    int lev=0;
+    while (u(e) && lev<maxheight)
+        lev++;
+    return lev;
+}
+
