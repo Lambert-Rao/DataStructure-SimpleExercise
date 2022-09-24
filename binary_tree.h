a7c12d9de8effeed972fda635d23910d2040918b
Lambert <lambert-teddy-smith@outlook.com>
2022-09-24 09:12:26 +0800

    binary tree is too easy

--- binary_tree.h
+++ binary_tree.h
@@ -3,70 +3,108 @@
 //
 
 #pragma once
+
 #include <iostream>
 #include <queue>
 
-template <typename T>
+template<typename T>
 class binaryTree;
 
-template <typename T>
-class binaryTreeNode {
+template<typename T>
+class binaryTreeNode
+{
     friend class binaryTree<T>;
+
     T element;
-    using ptr=binaryTreeNode<T> *;
+    using ptr = binaryTreeNode<T> *;
     binaryTreeNode<T> *left;
     binaryTreeNode<T> *right;
-    binaryTreeNode() : left(nullptr), right(nullptr) {}
-    explicit binaryTreeNode(T data) : element(data), left(nullptr), right(nullptr) {}
+
+    binaryTreeNode() : left(nullptr), right(nullptr)
+    {}
+
+    explicit binaryTreeNode(T data) : element(data), left(nullptr), right(nullptr)
+    {}
+
     binaryTreeNode(T data, binaryTreeNode<T> *left, binaryTreeNode<T> *right) :
-            element(data), left(left), right(right) {}
+            element(data), left(left), right(right)
+    {}
 };
 
-template <typename T>
-class binaryTree {
-    using ptr=binaryTreeNode<T> *;
+template<typename T>
+class binaryTree
+{
 public:
-    binaryTree() : root(nullptr) {}
-    explicit binaryTree(T data) : root(new binaryTreeNode<T>(data)) {}
+    using ptr = binaryTreeNode<T> *;
+
+    binaryTree() : root(nullptr)
+    {}
+
+    explicit binaryTree(T data) : root(new binaryTreeNode<T>(data))
+    {}
+
     binaryTree(T data, binaryTree<T> &left, binaryTree<T> &right) :
-            root(new binaryTreeNode<T>(data, left.root, right.root)) {
+            root(new binaryTreeNode<T>(data, left.root, right.root))
+    {
         left.root = nullptr;
         right.root = nullptr;
     }
-    ~binaryTree() { clear(); }
-    void clear() { clear(root); }
-    void preOrder() { preOrder(root); }
-    void inOrder() { inOrder(root); }
-    void postOrder() { postOrder(root); }
+
+    ~binaryTree()
+    { clear(); }
+
+    void clear()
+    { clear(root); }
+
+    void preOrder()
+    { preOrder(root); }
+
+    void inOrder()
+    { inOrder(root); }
+
+    void postOrder()
+    { postOrder(root); }
+
     void levelOrder();
-    int height() { return height(root); }
-    int size() { return size(root); }
-    bool empty() { return root == nullptr; }
+
+    int height()
+    { return height(root); }
+
+    int size()
+    { return size(root); }
+
+    bool empty()
+    { return root == nullptr; }
+
     void insert(T x);
-    void remove(T x);
-    void remove(ptr &t, T x);
+
+    void remove(T x)=delete;
+
+
     void destroy(ptr &t);
-    void copy(ptr &t, ptr other);
-    void copyTree(binaryTree<T> &other);
-    binaryTree<T> &operator=(binaryTree<T> &other);
-    void printTree();
-    void printTree(ptr t, int level);
+
 private:
     ptr root;
-    void clear(ptr &t);
+
+    ptr find(const T &x) const;
+    ptr search(const T &x);
     void preOrder(ptr t);
+
     void inOrder(ptr t);
+
     void postOrder(ptr t);
+
     int height(ptr t);
+
     int size(ptr t);
 };
 
 template<typename T>
 void binaryTree<T>::preOrder(binaryTree::ptr t)
 {
-    if(t!= nullptr)
+    if (t != nullptr)
     {
-        std::cout<<t->element<<std::endl;
+        std::cout << t->element << std::endl;
         preOrder(t->left);
         preOrder(t->right);
     }
@@ -76,10 +114,10 @@ void binaryTree<T>::preOrder(binaryTree::ptr t)
 template<typename T>
 void binaryTree<T>::inOrder(binaryTree::ptr t)
 {
-    if(t!= nullptr)
+    if (t != nullptr)
     {
         inOrder(t->left);
-        std::cout<<t->element<<std::endl;
+        std::cout << t->element << std::endl;
         inOrder(t->right);
     }
 
@@ -88,33 +126,32 @@ void binaryTree<T>::inOrder(binaryTree::ptr t)
 template<typename T>
 void binaryTree<T>::postOrder(binaryTree::ptr t)
 {
-    if(t!= nullptr)
+    if (t != nullptr)
     {
         postOrder(t->left);
         postOrder(t->right);
-        std::cout<<t->element<<std::endl;
+        std::cout << t->element << std::endl;
     }
 }
 
 template<typename T>
 void binaryTree<T>::levelOrder()
 {
-    auto t=root;
+    auto t = root;
     std::queue<ptr> q;
-    while(t!= nullptr)
+    while (t != nullptr)
     {
-        std::cout<<t->element<<std::endl;
-        if(t->left!= nullptr)
+        std::cout << t->element << std::endl;
+        if (t->left != nullptr)
             q.push(t->left);
-        if(t->right!= nullptr)
+        if (t->right != nullptr)
             q.push(t->right);
-        if(!q.empty())
+        if (!q.empty())
         {
-            t=q.front();
+            t = q.front();
             q.pop();
-        }
-        else
-            t= nullptr;
+        } else
+            t = nullptr;
     }
 
 }
@@ -122,22 +159,82 @@ void binaryTree<T>::levelOrder()
 template<typename T>
 int binaryTree<T>::height(binaryTree::ptr t)
 {
-    if(t== nullptr)
+    if (t == nullptr)
         return 0;
     else
     {
-        int l=height(t->left);
-        int r=height(t->right);
-        return l>r?l+1:r+1;
+        int l = height(t->left);
+        int r = height(t->right);
+        return l > r ? l + 1 : r + 1;
     }
 }
 
 template<typename T>
 int binaryTree<T>::size(binaryTree::ptr t)
 {
-    if(t== nullptr)
+    if (t == nullptr)
         return 0;
     else
-        return size(t->left)+size(t->right)+1;
+        return size(t->left) + size(t->right) + 1;
+}
+
+template<typename T>
+void binaryTree<T>::insert(T x)
+{
+    auto p = search(x);
+    if(p->element == x)
+        return;
+    else if(p->element<x)
+        p->right = new binaryTreeNode<T>(x);
+    else
+        p->left = new binaryTreeNode<T>(x);
+}
+
+
+template<typename T>
+binaryTreeNode<T> *binaryTree<T>::find(const T &x) const
+{
+    auto t = root;
+    while (t != nullptr)
+    {
+        if (t->element == x)
+            return t;
+        else if (t->element > x)
+            t = t->left;
+        else
+            t = t->right;
+    }
+    return nullptr;
+}
+
+template<typename T>
+binaryTreeNode<T> * binaryTree<T>::search(const T &x)
+{
+    auto t = root;
+    auto p(t);
+    while (t != nullptr)
+    {
+         p = t;
+        if (t->element == x)
+            return t;
+        else if (t->element > x)
+            t = t->left;
+        else
+            t = t->right;
+    }
+    return p;
+}
+
+template<typename T>
+void binaryTree<T>::destroy(binaryTree::ptr &t)
+{
+    if (t != nullptr)
+    {
+        destroy(t->left);
+        destroy(t->right);
+        delete t;
+        t = nullptr;
+    }
+
 }
 
